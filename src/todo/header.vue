4b646ee107fda629e715f835e3f714302e4d91d6
gq <guanqinghz@163.com>
2019-07-08 22:22:33 +0800

    todo 业务，打包优化

--- src/todo/header.vue
+++ src/todo/header.vue
@@ -1,13 +1,17 @@
 <template>
-    
+    <header class="main-header">
+        <h1>Jtodo</h1>
+    </header>
 </template>
 
-<script>
-    export default {
-        name: "header"
+<style lang="stylus" scoped>
+.main-header{
+    text-align center
+    h1{
+        font-size 100px
+        color: rgba(175, 47, 47, 0.4)
+        font-weight 100
+        margin 20px
     }
-</script>
-
-<style scoped>
-
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +10,7 @@ const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bound.js',
+        filename: 'bound.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -31,20 +32,6 @@ const config = {
                 use: [
                     'style-loader',
                     'css-loader'
-                ]
-            },
-            {
-                test: /\.styl/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
                 ]
             },
             {
@@ -73,6 +60,20 @@ const config = {
 }
 
 if (isDev){
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port: 8000,
@@ -87,6 +88,56 @@ if (isDev){
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[hash:8].css'),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'vendor'
+        // }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'tuntime'
+        // })
+    )
+    config.optimization = {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    chunks: 'initial',
+                    minChunks: 2, maxInitialRequests: 5,
+                    minSize: 0
+                },
+                vendor: {
+                    test: /node_modules/,
+                    chunks: 'initial',
+                    name: 'vendor',
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        },
+        runtimeChunk: true
+    }
 }
 
 module.exports = config
